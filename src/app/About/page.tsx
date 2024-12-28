d9ba9227915f3d567690c7d4372e318f5b9273af
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-12-28 14:35:42 +0500

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
       <p className='text-[16px] text-center font-normal font-serif'>Welcome to LifeStyle, your ultimate destination for lifestyle inspiration, practical advice, and a journey toward a more fulfilling life!
         At LifeStyle, we believe that life is an adventure, and its all about finding balance, embracing new experiences, and nurturing the best version of ourselves. Our blog covers a wide array of topics, 
         including wellness, health, travel, fashion, personal growth,
-        and much more. Whether you're looking for tips to live a healthier life, discover new travel spots, or simply get inspiration for a cozy home, we've got you covered!</p>
+        and much more. Whether you are looking for tips to live a healthier life, discover new travel spots, or simply get inspiration for a cozy home, we have got you covered!</p>
         <h1 className='text-3xl text-center font-bold p-10'>Our Mission</h1>
         <p className='text-[16px] text-center font-normal font-serif pb-10'>Our mission is to provide our readers with valuable, actionable, and insightful content that enhances their everyday lives.
          We want to help you live with purpose, spark creativity, and feel empowered in every aspect of your life, no matter where you are on your journey.
