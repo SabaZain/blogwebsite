bb809185af725e82dad5c3479be64f9a84d20615
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-12-30 19:18:27 +0500

    Update page.tsx

--- src/app/Hero/page.tsx
+++ src/app/Hero/page.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className='mt-8 mb-8'>
             <h1 className='text-[24px] text-center  font-bold p-3'>Embrace a life well-lived</h1>
             <p className='text-[16px] md:text-[18px] lg:text-[18px] text-center text-second font-semibold p-3'>Discover the perfect balance of health, adventure, and comfort.<br/> Live life to the fullest, every day.</p>
-            <div className="text-center">
+            <div>
             <Button variant="default" className='text-xl text-center mt-4 ml-40 md:ml-52 lg:ml-52 p-3'><Link href="Blog-Category">Blogs Category</Link></Button>
             </div>
         </div>
